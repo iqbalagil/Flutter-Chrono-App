15b1e550e298387e3e826863cbb79e162e7e440b
iqbal <iqbalagil@gmai.com>
2024-05-27 22:55:00 +0700

    feat: add image using storage firebase

--- lib/app/mainapp/main_view.dart
+++ lib/app/mainapp/main_view.dart
@@ -5,10 +5,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_application_akhir/common/color_common.dart';
 import 'package:flutter_application_akhir/firebase_options.dart';
 import 'package:flutter_application_akhir/app/mainapp/product_card.dart';
-
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:http/http.dart' as http;
 
 class MainView extends StatefulWidget {
   const MainView({Key? key}) : super(key: key);
+
   @override
   _MainViewState createState() => _MainViewState();
 }
@@ -28,7 +30,9 @@ class _MainViewState extends State<MainView> {
   void initState() {
     super.initState();
     initializeFirebase().then((_) {
-      fetchImageUrls();
+      downloadFilesInDirectory('product_imgaes').then((_) {
+        fetchImageUrls();
+      });
     });
   }
 
@@ -45,7 +49,7 @@ class _MainViewState extends State<MainView> {
   Future<void> fetchImageUrls() async {
     try {
       final firebase_storage.ListResult result =
-          await storage.ref().child('product_imgaes').listAll(); // Use the correct path
+          await storage.ref().child('product_imgaes').listAll();
       final futures = result.items.map((ref) async {
         final url = await ref.getDownloadURL();
         return url;
@@ -57,23 +61,45 @@ class _MainViewState extends State<MainView> {
     }
   }
 
+  Future<void> downloadFilesInDirectory(String directoryPath) async {
+    final firebase_storage.FirebaseStorage storage =
+        firebase_storage.FirebaseStorage.instance;
+
+    try {
+      final firebase_storage.ListResult result =
+          await storage.ref().child(directoryPath).listAll();
+
+      for (final firebase_storage.Reference ref in result.items) {
+        if (ref.fullPath != null) {
+          final url = await ref.getDownloadURL();
+          print('Downloading file from $url');
+          // Perform the download operation here
+          // For example, you can use http package to download the file
+        }
+      }
+    } catch (e) {
+      print('Error downloading files: $e');
+    }
+  }
+
   void _initializeProductData() {
-    products = imageUrls.map((imageUrl) {
-      // Generate dummy product data
+    products = imageUrls.asMap().entries.map((entry) {
+      final index = entry.key;
+      final imageUrl = entry.value;
+      final categories = ['Rolex', 'Patek Philippe', 'Omega'];
       return {
-        'name': 'Blue Eyed',
-        'category': 'Rolex',
+        'name': 'Product ${index + 1}',
+        'category': categories[index % categories.length],
         'priceRange': r'$300 - $500',
-        'description': 'This is a sample product description.',
+        'description': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
         'image_url': imageUrl,
       };
     }).toList();
 
     setState(() {
-      isLoading = false; 
+      isLoading = false;
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -87,44 +113,50 @@ class _MainViewState extends State<MainView> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-              // Profile Header
-              ListTile(
-                leading: const CircleAvatar(
-                  backgroundImage: NetworkImage('https://via.placeholder.com/150'), // Placeholder image
-                ),
-                title: Text('Welcome back, $userName!', style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                subtitle: const Text('Explore our latest collection!'),
-              ),
-              SizedBox(height: 30),
-
-              // Image Carousel
-              CarouselSlider(
-                items: imageUrls.map((url) => 
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.network(url, fit: BoxFit.cover, width: 1000.0),
-                  )
-                ).toList(),
-                options: CarouselOptions(height: 200, enlargeCenterPage: true, autoPlay: true),
-              ),
-              SizedBox(height: 30),
-
-              // Category Buttons
-              Text('Categories', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-              SizedBox(height: 10),
-              SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: Row(
-                  children: [
-                    _buildCategoryButton('All'),
-                    _buildCategoryButton('Rolex'),
-                    _buildCategoryButton('Patek Philippe'),
-                    _buildCategoryButton('Omega'),
-                  ],
-                ),
-              ),
-
-GridView.builder(
+                    ListTile(
+                      leading: const CircleAvatar(),
+                      title: Text(
+                        'Welcome back, $userName!',
+                        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: const Text('Explore our latest collection!'),
+                    ),
+                    const SizedBox(height: 30),
+                    CarouselSlider(
+                      items: imageUrls.map((url) {
+                        return ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: CachedNetworkImage(
+                            imageUrl: url,
+                            fit: BoxFit.cover,
+                            width: 1000.0,
+                            placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
+                            errorWidget: (context, url, error) => const Icon(Icons.error),
+                          ),
+                        );
+                      }).toList(),
+                      options: CarouselOptions(
+                        height: 200,
+                        enlargeCenterPage: true,
+                        autoPlay: true,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    const Text('Categories', style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 10),
+                    SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: [
+                          _buildCategoryButton('All'),
+                          _buildCategoryButton('Rolex'),
+                          _buildCategoryButton('Patek Philippe'),
+                          _buildCategoryButton('Omega'),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    GridView.builder(
                       shrinkWrap: true,
                       physics: const NeverScrollableScrollPhysics(),
                       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
@@ -135,9 +167,9 @@ GridView.builder(
                       itemCount: products.length,
                       itemBuilder: (context, index) {
                         final product = products[index];
-                        if (selectedCategory == 'all' || product['category'] == selectedCategory) {
+                        if (selectedCategory == 'all' || product['category'].toLowerCase() == selectedCategory.toLowerCase()) {
                           return ProductCard(
-                            imageUrl: product['image_url'],
+                            imagePath: product['image_url'],
                             productName: product['name'],
                             priceRange: product['priceRange'],
                             description: product['description'],
@@ -154,16 +186,25 @@ GridView.builder(
     );
   }
 
-Widget _buildCategoryButton(String label) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-      margin: EdgeInsets.only(right: 10),
-      decoration: BoxDecoration(
-        color: selectedCategory == label.toLowerCase() ? TColor.primaryColor1 : Colors.white,
-        borderRadius: BorderRadius.circular(25),
-      ),
-      child: Text(label, style: TextStyle(
-          color: selectedCategory == label.toLowerCase() ? Colors.white : Colors.black
+  Widget _buildCategoryButton(String label) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedCategory = label.toLowerCase();
+        });
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        margin: const EdgeInsets.only(right: 10),
+        decoration: BoxDecoration(
+          color: selectedCategory == label.toLowerCase() ? TColor.primaryColor1 : Colors.white,
+          borderRadius: BorderRadius.circular(25),
+        ),
+        child: Text(
+          label,
+          style: TextStyle(
+            color: selectedCategory == label.toLowerCase() ? Colors.white : Colors.black,
+          ),
         ),
       ),
     );
--- lib/app/mainapp/product_card.dart
+++ lib/app/mainapp/product_card.dart
@@ -1,15 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:cached_network_image/cached_network_image.dart';
 
 class ProductCard extends StatelessWidget {
-  final String imageUrl;
+  final String imagePath;
   final String productName;
-  final String priceRange; // e.g., "300\$ - 500\$"
+  final String priceRange;
   final String description;
 
-  ProductCard({required this.imageUrl, required this.productName, required this.priceRange, required this.description});
+  const ProductCard({
+    Key? key,
+    required this.imagePath,
+    required this.productName,
+    required this.priceRange,
+    required this.description,
+  }) : super(key: key);
+
+  // Fetch image URL from Firebase Storage
+  Future<String> _getImageUrl(String imagePath) async {
+    try {
+      final ref = firebase_storage.FirebaseStorage.instance.ref().child(imagePath);
+      return await ref.getDownloadURL();
+    } catch (e) {
+      print('Error fetching image: $e');
+      return ''; // Return an empty string if there's an error
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    return FutureBuilder<String>(
+      future: _getImageUrl(imagePath),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator()); // Loading indicator
+        } else if (snapshot.hasError || !snapshot.hasData || snapshot.data!.isEmpty) {
+          return _buildCard('assets/images/placeholder.jpg'); // Error or empty data placeholder
+        } else {
+          final imageUrl = snapshot.data!;
+          return _buildCard(imageUrl);
+        }
+      },
+    );
+  }
+
+  Widget _buildCard(String imageUrl) {
     return Card(
       elevation: 2,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
@@ -17,37 +52,46 @@ class ProductCard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           ClipRRect(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(10),
-              topRight: Radius.circular(10),
-            ),
-            child: Image.network(
-              imageUrl,
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(10)),
+            child: CachedNetworkImage(
+              imageUrl: imageUrl,
               height: 150,
               width: double.infinity,
               fit: BoxFit.cover,
+              placeholder: (context, url) => const Center(child: CircularProgressIndicator()),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(productName, style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-                SizedBox(height: 5),
-                Text(priceRange),
-                SizedBox(height: 10),
-                // Rolex-style description
-                Text(description, style: TextStyle(fontFamily: 'Roboto', fontSize: 14)),
-                SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
+          Expanded( // Make the text content scrollable
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    IconButton(icon: Icon(Icons.bookmark_border), onPressed: () {}),
-                    ElevatedButton(onPressed: () {}, child: Text('Buy Now')),
+                    Text(productName, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 5),
+                    Text(priceRange),
+                    const SizedBox(height: 10),
+                    Text(
+                      description,
+                      style: const TextStyle(fontFamily: 'Roboto', fontSize: 14),
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    const SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        IconButton(icon: const Icon(Icons.bookmark_border), onPressed: () {}),
+                        Expanded( // Wrap the Row with Expanded
+                          child: ElevatedButton(onPressed: () {}, child: const Text('Buy Now')),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
-              ],
+              ),
             ),
           ),
         ],
